68c464b06bd780a5709b9fc834073957f607f2f7
Le Trung <trungle30902@gmail.com>
2022-12-15 23:25:54 +0700

    [221215] 🐹 - start project

---
+++ src/main/java/com/trungle/littlepetshop/LittlePetShopApplication.java
@@ -0,0 +1,13 @@
+package com.trungle.littlepetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlePetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittlePetShopApplication.class, args);
+    }
+
+}
